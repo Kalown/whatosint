06b5959f7bdecdca25d8a03c293e6ec8d50911ab
Kal0wn <86701829+Kalown@users.noreply.github.com>
2022-03-07 07:48:50 +0530

    Update check.bat

--- check.bat
+++ check.bat
@@ -1,7 +1,7 @@
 @echo off
-C:\Program Files\BraveSoftware\Brave-Browser\Application 
+C:
+cd C:\Program Files\BraveSoftware\Brave-Browser\Application 
 :a
 set /p num=Please Enter the number you want to check [International Format] :
 brave.exe https://web.whatsapp.com/send?phone=%2B%num%
-timeout /t 3 /nobreak > NUL
 goto a
